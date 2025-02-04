de2c62b6d5b8b6ce8bdf8891ce2b478213da139e
kudzinp <gavrilkoo@gmail.com>
2025-02-04 12:51:16 +0100

    added updating of completion

--- ToDoList/ViewModels/ListViewModel.swift
+++ ToDoList/ViewModels/ListViewModel.swift
@@ -36,4 +36,10 @@ class ListViewModel: ObservableObject {
     func moveItem(from: IndexSet, to: Int) {
         items.move(fromOffsets: from, toOffset: to)
     }
+    
+    func updateItem(item: ItemModel) {
+        if let index = items.firstIndex(where: { $0.id == item.id }) {
+            items[index] = item.updateCompletion()
+        }
+    }
 }
