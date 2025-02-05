a08fbaefb6aea661489e6a74015dd240774d538f
kudzinp <gavrilkoo@gmail.com>
2025-02-05 21:22:03 +0100

    added UserDefaults

--- ToDoList/ViewModels/ListViewModel.swift
+++ ToDoList/ViewModels/ListViewModel.swift
@@ -9,19 +9,24 @@ import Foundation
 
 class ListViewModel: ObservableObject {
     
-    @Published var items: [ItemModel] = []
+    @Published var items: [ItemModel] = [] {
+        didSet {
+            saveItems()
+        }
+    }
+    
+    let itemsKey: String = "items_list"
     
     init() {
         getItems()
     }
     
     func getItems() {
-        let newItems = [
-            ItemModel(title: "First", isCompleted: true),
-            ItemModel(title: "Second", isCompleted: false),
-            ItemModel(title: "Third", isCompleted: true)
-        ]
-        items.append(contentsOf: newItems)
+        guard
+            let data = UserDefaults.standard.data(forKey: itemsKey),
+            let savedItems = try? JSONDecoder().decode([ItemModel].self, from: data)
+        else { return }
+        self.items = savedItems
     }
     
     func addItem(title: String) {
@@ -40,6 +45,12 @@ class ListViewModel: ObservableObject {
     func updateItem(item: ItemModel) {
         if let index = items.firstIndex(where: { $0.id == item.id }) {
             items[index] = item.updateCompletion()
+        }
+    }
+    
+    func saveItems() {
+        if let encodedData = try? JSONEncoder().encode(items) {
+            UserDefaults.standard.set(encodedData, forKey: itemsKey)
         }
     }
 }
